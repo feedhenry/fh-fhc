b8076416140ff29205df94202849a4f0d4ffb889
craig.brookes <craig.brookes@feedhenry.com>
2012-04-03 11:55:31 +0100

    change to native to nativecfg as native is a reserved word in node 0.4

--- test/test_native.js
+++ test/test_native.js
@@ -1,7 +1,7 @@
 //tests for native command
 var assert = require('assert');
 var fhc = require("../lib/fhc.js");
-var native = require('../lib/native');
+var nat = require('../lib/nativecfg.js');
 var conf = require("../lib/fhcfg.js");
 var apps = require("../lib/apps.js");
 var request = require('../lib/utils/request.js');
@@ -18,7 +18,7 @@ module.exports = {
         fhc.load(function (er){
             request.requestFunc = mockrequest.mockRequest;
             if(ini.get("feedhenry") === undefined)ini.set("feedhenry","https://apps.feedhenry.com");
-            native(["config=apple","app="+testguid],function (err, data){
+            nat(["config=apple","app="+testguid],function (err, data){
                     assert.equal(err,null);
                     assert.equal(data.substr(0,5), "<?xml");
             });
@@ -29,7 +29,7 @@ module.exports = {
         fhc.load(function (er){
             //endure we have a domain to read against
             if(ini.get("feedhenry") === undefined)ini.set("feedhenry","https://apps.feedhenry.com");
-            native(["config=apple","app="+testguid,"dir="+writeDir],function (err, data){
+            nat(["config=apple","app="+testguid,"dir="+writeDir],function (err, data){
                 console.log(data);
                 assert.equal(err,null);
                 assert.equal(data.substr(0,21), "native config written");
@@ -39,7 +39,7 @@ module.exports = {
 
     "test no fail when write fails":function (){
         fhc.load(function (er){
-            native(["config=apple","app="+testguid,"dir=somedir"],function (err, data){
+            nat(["config=apple","app="+testguid,"dir=somedir"],function (err, data){
                 assert.equal(err,null);
                 //should still write out the contents to the terminal
                 assert.isNotNull(data);
