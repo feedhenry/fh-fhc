e0de5af13b0cd5ae133eed33ecf79fa35d487586
cianclarke <cian@cianclarke.com>
2014-10-28 13:33:20 -0400

    Test coverage for build, logs

---
+++ test/fixtures/app/logs.js
@@ -0,0 +1,19 @@
+var nock = require('nock');
+var assert = require('assert');
+
+var logReply = function(){
+  return {
+    "log": {
+      "stderr": "",
+      "stdout": "App started at: Tue Oct 28 2014 00:10:15 GMT+0000 (UTC) on port: 8278\n"
+    },
+    "status": "ok"
+  };
+};
+
+module.exports = nock('https://apps.feedhenry.com')
+.filteringRequestBody(function(path) {
+  return '*';
+})
+.post('/box/srv/1.1/ide/apps/app/logs', '*')
+.reply(200, logReply, { 'Content-Type': 'application/json' });
