87df0d75e251db3417aa49c856cd5bf35f60d26e
cianclarke <cian@cianclarke.com>
2014-11-12 16:21:59 -0500

    Changes to output doxy index file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,13 @@ module.exports = function(grunt) {
   var packageVersion = pkgjs.version.replace('BUILD-NUMBER', buildNumber);
   var releaseDir = pkgName + '-' + packageVersion;
   var releaseFile = pkgName + '-' + packageVersion + '.tar.gz';
+  var path = require('path');
+  var fs = require('fs-extra');
+  var async = require('async');
+  var _ = require('underscore');
+  var underscoreDeepExtend = require('underscore-deep-extend');
+  _.mixin({deepExtend: underscoreDeepExtend(_)});
+
 
 
   // Project Configuration
@@ -92,6 +99,19 @@ module.exports = function(grunt) {
           'sed -i -e s/BUILD-NUMBER/' + buildNumber + '/ ' + outputDir + '/' + releaseDir + '/package.json',
           'tar -czf ' + distDir + '/' + releaseFile + ' -C ' + outputDir + ' ' + releaseDir
         ].join('&&')
+      },
+      docsToDoxy : {
+        options : {
+          stdout: true,
+          stderr: true,
+          failOnError: false
+        }, 
+        command : [
+          'cp -rf doc/fh3/ ../fh-doxy/public/dev_tools/fhc/',
+          'cp -rf doc/common/ ../fh-doxy/public/dev_tools/fhc/',
+          'cp -rf doc/fhc/ ../fh-doxy/public/dev_tools/fhc/',
+          'cp doc/index.md ../fh-doxy/public/dev_tools/fhc.md',
+        ].join('&&')
       }
     },
     plato: {
@@ -130,19 +150,96 @@ module.exports = function(grunt) {
   grunt.registerTask('dist', ['shell:clean', 'shell:dist']);
   grunt.registerTask('clean', ['shell:clean']);
   
-  grunt.registerTask('docs', function(){
+  grunt.registerTask('docs', ['docs-generate', 'docs-index', 'shell:docsToDoxy']);
+  grunt.registerTask('docs-index', function(){
+    var fhc = require('./lib/fhc.js');
+    
+    /*
+      Generates the contents of an index.html file for the tree of (fh3) commands, nicely formatted
+     */
+    function _genIndex(tree, level, parent){
+      var output = [],
+      recursors = [], // keep these until last in any list so the .cols look nice
+      keys = Object.keys(tree),
+      indent = Array(level).join('\t'),
+      colSpan = (level <= 2) ? 12 : (12/level),
+      innerColSpan = 12/colSpan+2;
+      
+      if (level > 1) {
+        output.push(indent + '<div class="col-md-' + colSpan + '">');  
+      }
+      output.push(indent + '\t<h3>' + tree._groupName + '</h3>');  
+      
+      for (var i=0; i<keys.length; i++){
+        var key = keys[i],
+        cmd = tree[key],
+        cmdPath = cmd._path, 
+        name = cmd._cmdName;
+        
+        if (key[0] === '_'){
+          continue;
+        }
+        
+        // Add an entry to our index.md file
+        if (typeof cmd === 'object'){
+          // recurse
+          var recurseRes = _genIndex(cmd, level+1);
+          // For 1st-level commands, we want to make sure the sub-commands appear last in the list
+          if (level === 1){
+            recursors = recursors.concat(recurseRes);  
+          }else{
+            output = output.concat(recurseRes);  
+          }
+        }else{
+          // First check if a docs article exists for this command - if not, let's not write it to this index..
+          var docMdPath = path.join('doc', cmdPath.replace(/\.js$/, '.md'));
+          if (!fs.existsSync(docMdPath)){
+            console.log('Warning: No docs file found for ' + docMdPath);
+            continue;
+          }
+          cmdPath = cmd._path.replace(/\.js$/, '.html');
+          cmdPath = cmdPath.split('/');
+          cmdPath.shift(1);
+          cmdPath = cmdPath.join('/');
+          // pop off the 'common', 'fh2', 'fh3' parts..
+          
+          output.push(indent + '\t<a class="col-md-' + innerColSpan + '" href="fhc/' + cmdPath + '">' + name + '</a>');  
+        }
+      }
+      output = output.concat(recursors);
+      if (level > 1) {
+        output.push(indent + '</div>');
+      }
+      return output;
+    }
+    var treeForDoxy = _.deepExtend({}, fhc._tree.fh3, fhc._tree.common, fhc._tree.fhc),
+    indexOutput = _genIndex(treeForDoxy, 1),
+    docsDir = path.join(__dirname, 'doc'),
+    indexFile = path.join(docsDir, 'index.md');
+    indexOutput = [
+    '<h1>FHC - FeedHenry Command Line Interface API</h1>',
+    '<div class="alert alert-info"><strong>Note: </strong> This API Reference is for version ' + fhc._version.replace('-BUILD-NUMBER', ''),
+    'of FHC. To ensure you get the most relevant help for the version of FHC you have installed, the <code>fhc help</code> command can be used.',
+    'See <a href="https://github.com/feedhenry/fh-fhc#usage">https://github.com/feedhenry/fh-fhc#usage</a> for usage.',
+    'To find the version of fhc you have installed, use the <code>fhc version</code> command</div>'
+    ].concat(indexOutput);
+    // for docs, we're only interested in FH3 commands & don't want to talk about fh2 or internal ones
+    
+    fs.writeFileSync(indexFile, indexOutput.join('\n'));
+    
+  });
+  
+  grunt.registerTask('docs-generate', function(){
     var fhc = require('./lib/fhc.js'),
     help = require('./lib/cmd/fhc/help.js'),
-    fs = require('fs-extra'),
-    path = require('path'),
-    async = require('async');
+    docsDir = path.join(__dirname, 'doc');
     
-    function writeDocFile(usage, cmdPath, cb){
-      var splitPath = cmdPath.split(path.sep);
-      splitPath = splitPath.join(path.sep);
-      var writeTo = path.join(__dirname, 'doc', splitPath);
-      writeTo = writeTo.replace(/\.js$/, '.md'); // Replace the JS extension with that of a markdown file
-      
+    function writeDocFile(usage, cmd, cb){
+      var cmdPath = cmd._path,
+      writeTo;
+      cmdPath = cmdPath.replace(/\.js$/, '.md'); // Replace the JS extension with that of a markdown file
+      writeTo = path.join(docsDir, cmdPath),  
+      // now write the file
       fs.outputFile(writeTo, usage, cb);
     }
     
@@ -161,7 +258,7 @@ module.exports = function(grunt) {
                 if (err){
                   return cb(err);
                 }
-                return writeDocFile(usage, safeClosureCmd._path, cb);
+                return writeDocFile(usage, safeClosureCmd, cb);
               });  
             });  
           })(cmd);
@@ -177,7 +274,8 @@ module.exports = function(grunt) {
     var done = this.async();
     
     fhc.load(function(conf){
-      var writers = genDocs(fhc._tree, done);
+      var tree = fhc._tree,
+      writers = genDocs(tree, done);
       async.parallel(writers, done);      
     });
   });
--- doc/fhc/fhc.md
+++ doc/fhc/fhc.md
@@ -1,17 +1,61 @@
-fhc(1) -- FeedHenry CLI.
-========================
+## FHC
 
-## SYNOPSIS
+### Dependencies
+
+Install node.js &amp; npm.
+
+Follow instructions on <a href="http://nodejs.org/">nodejs.org</a> for your OS. This will add 2 command line applications: <code>node</code> and <code>npm</code>
+
+###Install FHC
+Execute the following in a terminal/command prompt:
+
+```
+npm install -g fh-fhc
+```
+
+If installing on Linux, you may need to run this command as a sudoer:
+
+```
+sudo npm install -g fh-fhc
+```
+
+`fhc` will now be available from your command line (if you use zsh, do a `hash -r` to pick it up)
+
+To test FHC is installed correctly, and to show the version you have installed, use:
+
+```
+fhc -v
+```
+
+<span style="font-size: 23px;line-height: 35px">Command Completion (Linux and Mac only)</span>
+The FHC bash completion script allows Tab completion of the various FHC commands, including completing app identifiers when performing actions on a single app. To install the FHC bash completion script, execute the following:
+
+```
+fhc completion >> ~/.bashrc
+```
+
+If you're using an alternative shell to bash, you should append the output of <code>fhc completion</code> to the relevant file e.g. ~/.zshrc for zsh</p>
+
+### Usage
 
     fhc <command> [args]
 
-## DESCRIPTION
 
-fhc is the command line interface to FeedHenry.
+Use `fhc help` for general help, or `fhc help [command]` for help on a specific command.
 
-Run `fhc help` to get a list of available commands.
+To get started, set the target and then login:
+```
+fhc target https://[your-studio-domain].feedhenry.com
+fhc login [email address] [password]
+```
 
-## CONFIGURATION
+To list your projects, use:
+
+```
+fhc projects
+```
+
+### Configuration
 
 fhc is extremely configurable.  It reads its configuration options from
 5 places.
