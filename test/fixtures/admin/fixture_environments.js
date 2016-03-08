faa00062b013f919666e1a04b74bd3a3b1ffb913
Paolo Bueno <paolohaji@gmail.com>
2016-03-08 17:25:43 -0300

    Move environment fixtures to separate file and implement fix for autodeploy flags

--- test/fixtures/admin/fixture_environments.js
+++ test/fixtures/admin/fixture_environments.js
@@ -1,30 +1,23 @@
 var nock = require('nock');
+var data = require('./environments');
 
-var envReplies = {
-  crud : function(){
-    return { 
-      status : 'ok',
-      _id : '1a',
-      label : 'My env',
-      targets : ['http://www.1.com', 'http://www.2.com']
-    };
-  },
-  list : function(){
-    return [envReplies.crud()];
-  }
-};
+function echoWithOk(uri, body) {
+  var parsed = JSON.parse(body);
+  parsed.status = 'ok';
+  return parsed;
+}
 
 module.exports = nock('https://apps.feedhenry.com')
 .filteringRequestBody(function() {
   return '*';
 })
 .post('/api/v2/environments', '*')
-.reply(200, envReplies.crud)
+.reply(200, echoWithOk)
 .get('/api/v2/environments/1a', '*')
-.reply(200, envReplies.crud)
+.reply(200, data[0])
 .put('/api/v2/environments/1a', '*')
-.reply(200, envReplies.crud)
+.reply(200, echoWithOk)
 .delete('/api/v2/environments/1a', '*')
-.reply(200, envReplies.crud)
+.reply(200, data[0])
 .get('/api/v2/environments/all', '*')
-.reply(200, envReplies.list);
+.reply(200, data);
