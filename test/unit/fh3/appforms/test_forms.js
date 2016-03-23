c95ab0c3eb5af7c918f79c308fe78f367ea49770
Paolo Bueno <paolohaji@gmail.com>
2016-03-23 11:08:49 -0300

    Add test for appforms forms import

--- test/unit/fh3/appforms/test_forms.js
+++ test/unit/fh3/appforms/test_forms.js
@@ -1,13 +1,15 @@
 var assert = require('assert');
 var genericCommand = require('genericCommand');
 require('test/fixtures/appforms/fixture_forms');
+
 var appformsforms = {
   create : genericCommand(require('cmd/fh3/appforms/forms/create')),
   read : genericCommand(require('cmd/fh3/appforms/forms/read')),
   update : genericCommand(require('cmd/fh3/appforms/forms/update')),
   delete : genericCommand(require('cmd/fh3/appforms/forms/delete')),
   list : genericCommand(require('cmd/fh3/appforms/forms/list')),
-  clone: genericCommand(require('cmd/fh3/appforms/forms/clone'))
+  clone: genericCommand(require('cmd/fh3/appforms/forms/clone')),
+  import: genericCommand(require('cmd/fh3/appforms/forms/import'))
 };
 
 
@@ -48,6 +50,12 @@ module.exports = {
   },
   'test appforms-forms clone': function(cb) {
     appformsforms.clone({ id : 'someformid' }, function (err){
+      assert.equal(err, null);
+      return cb();
+    });
+  },
+  'test appforms-forms import': function(cb) {
+    appformsforms.import({file: 'test/fixtures/appforms/fixture_import.zip'}, function(err) {
       assert.equal(err, null);
       return cb();
     });
