479c1431d324c04f7cbdb447c62c9140b1f8ad96
Paolo Bueno <paolohaji@gmail.com>
2016-04-06 12:12:38 -0300

    Modify fhc according to backend changes for MBaaS targets

--- test/unit/fh3/admin/test_mbaas.js
+++ test/unit/fh3/admin/test_mbaas.js
@@ -6,13 +6,12 @@ var adminmbaas = {
   create : genericCommand(require('cmd/fh3/admin/mbaas/create')),
   read : genericCommand(require('cmd/fh3/admin/mbaas/read')),
   update : genericCommand(require('cmd/fh3/admin/mbaas/update')),
-  delete : genericCommand(require('cmd/fh3/admin/mbaas/delete')),
   list : genericCommand(require('cmd/fh3/admin/mbaas/list'))
 };
-var anmBaaS = {url : 'http://mbaas.com', servicekey : 'svckey', id : '1a2b', username : 'test', password : 'test'};
+var anMBaaS = {url : 'http://mbaas.com', servicekey : 'svckey', id : '1a2b', username : 'test', password : 'test'};
 module.exports = {
   'test admin-mbaas list': function(cb) {
-    adminmbaas.list(_.clone(anmBaaS), function (err, data){
+    adminmbaas.list(_.clone(anMBaaS), function (err, data){
       assert.equal(err, null);
       assert.equal(data.length, 1);
       assert.equal(data[0].url, 'http://www.mbaas.com');
@@ -20,27 +19,21 @@ module.exports = {
     });
   },
   'test admin-mbaas read': function(cb) {
-    adminmbaas.read(_.clone(anmBaaS), function (err, data){
+    adminmbaas.read(_.clone(anMBaaS), function (err, data){
       assert.equal(err, null);
       assert.equal(data.url, 'http://www.mbaas.com');
       return cb();
     });
   },
   'test admin-mbaas create': function(cb) {
-    adminmbaas.create(_.clone(anmBaaS), function (err, data){
+    adminmbaas.create(_.clone(anMBaaS), function (err, data){
       assert.equal(err, null);
       assert.equal(data.url, 'http://www.mbaas.com');
       return cb();
     });
   },
   'test admin-mbaas update': function(cb) {
-    adminmbaas.update(_.clone(anmBaaS), function (err){
-      assert.equal(err, null);
-      return cb();
-    });
-  },
-  'test admin-mbaas delete': function(cb) {
-    adminmbaas.delete(_.clone(anmBaaS), function (err){
+    adminmbaas.update(_.clone(anMBaaS), function (err){
       assert.equal(err, null);
       return cb();
     });
