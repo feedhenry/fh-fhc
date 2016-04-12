e6a730fa71deb8a0621bc7a962698dec627cc8e0
Paolo Bueno <paolohaji@gmail.com>
2016-04-12 15:42:31 -0300

    Update tests and fixtures on top of the new api

--- test/unit/fh3/admin/test_environments_commands.js
+++ test/unit/fh3/admin/test_environments_commands.js
@@ -2,18 +2,10 @@ var assert = require('assert');
 var create = require('cmd/fh3/admin/environments/create.js');
 var environments = require('test/fixtures/admin/environments');
 module.exports = {
-  'test admin environments create command': function (cb) {
-    // map fixture's fields to the ones that are going to come from the command line
-    environments = environments.map(function (env) {
-      env.id = env._id;
-      // command expects `targets` to be a csv string
-      env.targets = env.targets.join(',');
-      return env;
-    });
-
-    environments.forEach(function (env) {
-      create.preCmd(env, function (_, entity) {
-        assert.equal(entity._id, env._id);
+  'test admin environments create command': function(cb) {
+    environments.forEach(function(env) {
+      create.preCmd(env, function(_, entity) {
+        assert.equal(entity.id, env.id);
       });
     });
     return cb();
