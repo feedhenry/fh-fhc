b641af3d824e09c1d886241d1c88e331468945c8
Paolo Bueno <paolohaji@gmail.com>
2016-04-12 15:42:31 -0300

    Update tests and fixtures on top of the new api

--- test/fixtures/admin/fixture_environments.js
+++ test/fixtures/admin/fixture_environments.js
@@ -1,22 +1,16 @@
 var nock = require('nock');
 var data = require('./environments');
 
-function echoWithOk(uri, body) {
-  var parsed = JSON.parse(body);
-  parsed.status = 'ok';
-  return parsed;
-}
-
 module.exports = nock('https://apps.feedhenry.com')
 .filteringRequestBody(function() {
   return '*';
 })
 .post('/api/v2/environments', '*')
-.reply(200, echoWithOk)
+.reply(200, data[0])
 .get('/api/v2/environments/1a', '*')
 .reply(200, data[0])
 .put('/api/v2/environments/1a', '*')
-.reply(200, echoWithOk)
+.reply(200, data[0])
 .delete('/api/v2/environments/1a', '*')
 .reply(200, data[0])
 .get('/api/v2/environments/all', '*')
--- test/unit/fh3/admin/test_environments.js
+++ test/unit/fh3/admin/test_environments.js
@@ -2,21 +2,21 @@ var assert = require('assert');
 var genericCommand = require('genericCommand');
 require('test/fixtures/admin/fixture_environments');
 var adminenvironments = {
-  create : genericCommand(require('cmd/fh3/admin/environments/create')),
-  read : genericCommand(require('cmd/fh3/admin/environments/read')),
-  update : genericCommand(require('cmd/fh3/admin/environments/update')),
-  delete : genericCommand(require('cmd/fh3/admin/environments/delete')),
-  list : genericCommand(require('cmd/fh3/admin/environments/list'))
+  create: genericCommand(require('cmd/fh3/admin/environments/create')),
+  read: genericCommand(require('cmd/fh3/admin/environments/read')),
+  update: genericCommand(require('cmd/fh3/admin/environments/update')),
+  delete: genericCommand(require('cmd/fh3/admin/environments/delete')),
+  list: genericCommand(require('cmd/fh3/admin/environments/list'))
 };
 
 
 module.exports = {
   'test admin-environments list': function(cb) {
-    adminenvironments.list({}, function (err, data){
+    adminenvironments.list({}, function(err, data) {
       assert.equal(err, null);
       assert.equal(data.length, 2);
-      assert.equal(data[0].label, 'My env');
-      assert.equal(data[1].label, 'Another env');
+      assert.equal(data[0].label, 'lbl1');
+      assert.equal(data[1].label, 'lbl2');
 
       var table = data._table;
       assert.equal(table['0'][2], false);
@@ -28,30 +28,30 @@ module.exports = {
     });
   },
   'test admin-environments read': function(cb) {
-    adminenvironments.read({ id : '1a'}, function (err, data){
+    adminenvironments.read({ id: '1a'}, function(err, data) {
       assert.equal(err, null);
-      assert.equal(data.label, 'My env');
+      assert.equal(data.label, 'lbl1');
       return cb();
     });
   },
   'test admin-environments create': function(cb) {
-    adminenvironments.create({ label : 'My env', targets : '1,2,3', id : '1a' }, function (err, data){
+    adminenvironments.create({ label: 'lbl1', target: 'testTarget1', id: '1a' }, function(err, data) {
       assert.equal(err, null);
 
-      assert.deepEqual(data.targets, ['1', '2', '3']);
-      assert.equal(data.label, 'My env');
-      assert.equal(data._id, '1a');
+      assert.equal(data.target.id, 'testTarget1');
+      assert.equal(data.label, 'lbl1');
+      assert.equal(data.id, '1a');
       return cb();
     });
   },
   'test admin-environments update': function(cb) {
-    adminenvironments.update({ label : 'My env', targets : '1,2,3', id : '1a' }, function (err){
+    adminenvironments.update({ label: 'lbl1', target: 'testTarget1', id: '1a' }, function(err) {
       assert.equal(err, null);
       return cb();
     });
   },
   'test admin-environments delete': function(cb) {
-    adminenvironments.delete({ id : '1a' }, function (err){
+    adminenvironments.delete({ id: '1a' }, function(err) {
       assert.equal(err, null);
       return cb();
     });
