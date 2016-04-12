ac2090f60e2b122e4068db8fbdd2adb80adb3db7
Paolo Bueno <paolohaji@gmail.com>
2016-04-12 09:51:24 -0300

    Reenable `fhc admin mbaas delete`

--- test/unit/fh3/admin/test_mbaas.js
+++ test/unit/fh3/admin/test_mbaas.js
@@ -6,12 +6,13 @@ var adminmbaas = {
   create : genericCommand(require('cmd/fh3/admin/mbaas/create')),
   read : genericCommand(require('cmd/fh3/admin/mbaas/read')),
   update : genericCommand(require('cmd/fh3/admin/mbaas/update')),
+  delete : genericCommand(require('cmd/fh3/admin/mbaas/delete')),
   list : genericCommand(require('cmd/fh3/admin/mbaas/list'))
 };
 var anMBaaS = {url : 'http://mbaas.com', servicekey : 'svckey', id : '1a2b', username : 'test', password : 'test'};
 module.exports = {
   'test admin-mbaas list': function(cb) {
-    adminmbaas.list(_.clone(anMBaaS), function (err, data){
+    adminmbaas.list(_.clone(anMBaaS), function(err, data) {
       assert.equal(err, null);
       assert.equal(data.length, 1);
       assert.equal(data[0].url, 'http://www.mbaas.com');
@@ -19,21 +20,27 @@ module.exports = {
     });
   },
   'test admin-mbaas read': function(cb) {
-    adminmbaas.read(_.clone(anMBaaS), function (err, data){
+    adminmbaas.read(_.clone(anMBaaS), function(err, data) {
       assert.equal(err, null);
       assert.equal(data.url, 'http://www.mbaas.com');
       return cb();
     });
   },
   'test admin-mbaas create': function(cb) {
-    adminmbaas.create(_.clone(anMBaaS), function (err, data){
+    adminmbaas.create(_.clone(anMBaaS), function(err, data) {
       assert.equal(err, null);
       assert.equal(data.url, 'http://www.mbaas.com');
       return cb();
     });
   },
   'test admin-mbaas update': function(cb) {
-    adminmbaas.update(_.clone(anMBaaS), function (err){
+    adminmbaas.update(_.clone(anMBaaS), function(err) {
+      assert.equal(err, null);
+      return cb();
+    });
+  },
+  'test admin-mbaas delete': function(cb) {
+    adminmbaas.delete(_.clone(anMBaaS), function(err) {
       assert.equal(err, null);
       return cb();
     });
