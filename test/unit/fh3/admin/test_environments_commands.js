e33ff72f483c6d923492896e57509b83805fd62b
Aiden Keating <akeating@redhat.com>
2016-08-25 10:59:09 +0100

    RHMAP-9554 Update tests for environment target check
    Now that the environment `create` and `update` methods perform a check
    on the target type before performing an action the tests in fhc need
    to be updated to account for the new api calls being made.
    
    This adds extra calls to the `nock` interceptor list for checking the
    target type.

--- test/unit/fh3/admin/test_environments_commands.js
+++ test/unit/fh3/admin/test_environments_commands.js
@@ -4,6 +4,9 @@ var environments = require('test/fixtures/admin/environments');
 module.exports = {
   'test admin environments create command': function(cb) {
     environments.forEach(function(env) {
+      // Set target to `id` to avoid calls to /api/v2/mbaases/[Object] in
+      // check in `lib/utils/mbaas-token-check.js`
+      env.target = env.target.id;
       create.preCmd(env, function(_, entity) {
         assert.equal(entity.id, env.id);
       });
