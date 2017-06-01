bb5ab801f1f7648e098f3b0b4aa1790f541707f1
Camila Macedo <cmacedo@Camilas-MacBook-Pro.local>
2017-06-01 20:41:53 -0300

    FH-3618 - Remove support of V2 commands

--- test/fixtures/app/fixture_live_act.js
+++
@@ -1,9 +0,0 @@
-var nock = require('nock');
-module.exports = nock('https://apps-live.feedhenry.com')
-.filteringRequestBody(function() {
-  return '*';
-})
-.post('/cloud/somefunc')
-.reply(200, function() {
-  return { ok : true, status : 'ok', live : true};
-}, { 'Content-Type': 'application/json' });
--- test/unit/fh3/cloud/test_cloud.js
+++ test/unit/fh3/cloud/test_cloud.js
@@ -1,24 +1,15 @@
 var assert = require('assert');
 var genericCommand = require('genericCommand');
 var nockCloud2 = require('test/fixtures/app/fixture_cloud2');
-var nockHosts = require('test/fixtures/app/fixture_hosts')(1);
-var appReadNock = require('test/fixtures/app/fixture_appread.js')(1);
+var nockHosts = require('test/fixtures/app/fixture_hosts')(2);
+var appReadNock = require('test/fixtures/app/fixture_appread.js')(4);
 var cloud = genericCommand(require('cmd/fh3/app/cloud'));
 module.exports = {
-  setUp : function(cb) {
-    return cb();
-  },
   'test fh3 cloud': function(cb) {
     cloud({app : '1a', path : '/some/custom/cloud/host', 'data' : '', 'env' : 'development'}, function(err, data) {
       assert.equal(err, null, err);
       assert(data.ok === true);
       return cb();
     });
-  },
-  tearDown : function(cb) {
-    nockHosts.done();
-    nockCloud2.done();
-    appReadNock.done();
-    return cb();
   }
 };
