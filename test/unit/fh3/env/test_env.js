e28be331b9b29fb09568cead12750abd38a37d37
Camila Macedo <cmacedo@Camilas-MacBook-Pro.local>
2017-07-05 16:54:46 -0300

    RHMAP-16061 - Update FHC to honour the mask-value options for environment variables

--- test/unit/fh3/env/test_env.js
+++ test/unit/fh3/env/test_env.js
@@ -12,12 +12,14 @@ var envCmd = {
   delete: genericCommand(require('cmd/fh3/env/delete'))
 };
 
+
 module.exports = {
   'test env create --app=<app> --name=<name> --env=<environment> --json': function(cb) {
     envCmd.create({app:'1a', name:'name', env:'dev', json:true}, function(err, data) {
       assert.equal(err, null);
       assert.notEqual(data, null);
       assert.ok(data.status, "ok");
+      assert.equal(data._table, null);
       return cb();
     });
   },
@@ -25,6 +27,7 @@ module.exports = {
     envCmd.delete({app:'1a', id:'2b', env:'dev', json:true}, function(err, data) {
       assert.equal(err, null);
       assert.notEqual(data, null);
+      assert.equal(data._table, null);
       return cb();
     });
   },
@@ -35,6 +38,9 @@ module.exports = {
       assert.equal(table['0'][0], '2b');
       assert.equal(table['0'][2], 'assa');
       assert.equal(table['0'][3], 'test2');
+      assert.equal(table['1'][0], 'nqnbvq2u3lvmu2xnehvmtpca');
+      assert.equal(table['1'][2], 'Test2');
+      assert.equal(table['1'][3], '*');
       return cb();
     });
   },
@@ -56,6 +62,15 @@ module.exports = {
     envCmd.update({app:'1a', id:'2b', value:'value', env:'dev', json:true}, function(err, data) {
       assert.equal(err, null);
       assert.notEqual(data, null);
+      assert.equal(data._table, null);
+      return cb();
+    });
+  },
+  'test fhc env update --app=<app> --id=<id> --mask --json': function(cb) {
+    envCmd.update({app:'1a', id:'2b', mask:true, env:'dev', json:true}, function(err, data) {
+      assert.equal(err, null);
+      assert.notEqual(data, null);
+      assert.equal(data._table, null);
       return cb();
     });
   },
@@ -66,3 +81,5 @@ module.exports = {
     });
   }
 };
+
+
