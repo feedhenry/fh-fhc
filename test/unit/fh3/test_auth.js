aa0515206da258b2613a1e8d49aeabb8a4591036
Camila Macedo <cmacedo@Camilas-MacBook-Pro.local>
2017-08-20 14:48:03 +0100

    FH-3626 - Change the cmd auth to follow the V3 standard and add unit tests

---
+++ test/unit/fh3/test_auth.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var genericCommand = require('genericCommand');
+var command = genericCommand(require('cmd/fh3/auth'));
+
+var nock = require('nock');
+module.exports = nock('https://apps.feedhenry.com')
+  .post('/box/srv/1.1/admin/authpolicy/auth')
+  .reply(200, {status:"ok"});
+
+module.exports = {
+  'test appinit --app --key': function(cb) {
+    command({app:'xzauxfwgzpedqbcaxujffd6p',key:'934ba74c28076931633bbc41507462c6e9d85894'}, function(err,data) {
+      assert.equal(err, null);
+      assert.ok(data);
+      assert.equal(data.status, "ok");
+      return cb();
+    });
+  }
+};
