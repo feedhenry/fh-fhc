6aca12d395de52c199d3b87a26df4d8c1951ad87
Camila Macedo <cmacedo@Camilas-MacBook-Pro.local>
2017-09-19 14:17:02 +0100

    FH-3627 - Bump Version
    add --json

--- test/unit/fh3/test_build.js
+++ test/unit/fh3/test_build.js
@@ -6,7 +6,7 @@ var _ = require('underscore');
 
 module.exports = {
   'test build --project --app --cloud_app --tag --environment --destination': function(cb) {
-    command({project:'1a2b3c4d5e6f7g8e9f0a1b2c',app:'1a2b3c4d5e6f7g8e9f0a1b2d',cloud_app:'1a2b3c4d5e6f7g8e9f0a1b2e',tag:'1a2b3c4d5e6f7g8e9f0a1b2e', destination:'android', }, function(err,data) {
+    command({project:'1a2b3c4d5e6f7g8e9f0a1b2c',app:'1a2b3c4d5e6f7g8e9f0a1b2d',cloud_app:'1a2b3c4d5e6f7g8e9f0a1b2e',tag:'1a2b3c4d5e6f7g8e9f0a1b2e', destination:'android' }, function(err,data) {
       assert.equal(err, null, err);
       assert.ok(data.length === 1);
       data = _.first(data);
