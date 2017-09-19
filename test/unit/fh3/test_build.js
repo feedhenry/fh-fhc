d4905bdb8d79be0640e8d6a8a626dd4282fbd8a9
Camila Macedo <cmacedo@Camilas-MacBook-Pro.local>
2017-09-19 13:31:29 +0100

    FH-3627- Change the cmd  build to follow the V3 standard and add unit tests

--- test/unit/fh3/test_build.js
+++ test/unit/fh3/test_build.js
@@ -1,10 +1,12 @@
 var assert = require('assert');
+var genericCommand = require('genericCommand');
+var command = genericCommand(require('cmd/fh3/build'));
 var nockBuild = require('test/fixtures/app/fixture_build');
-var build = require('cmd/common/build');
 var _ = require('underscore');
+
 module.exports = {
-  'test common build': function(cb) {
-    build({_ : ['project=1a2b3c4d5e6f7g8e9f0a1b2c', 'app=1a2b3c4d5e6f7g8e9f0a1b2d', 'cloud_app=1a2b3c4d5e6f7g8e9f0a1b2e', 'tag=0.2.0', 'environment=testing', 'destination=android']}, function(err, data) {
+  'test build --project --app --cloud_app --tag --environment --destination': function(cb) {
+    command({project:'1a2b3c4d5e6f7g8e9f0a1b2c',app:'1a2b3c4d5e6f7g8e9f0a1b2d',cloud_app:'1a2b3c4d5e6f7g8e9f0a1b2e',tag:'1a2b3c4d5e6f7g8e9f0a1b2e', destination:'android', }, function(err,data) {
       assert.equal(err, null, err);
       assert.ok(data.length === 1);
       data = _.first(data);
@@ -12,7 +14,6 @@ module.exports = {
       data = _.first(data);
       assert.ok(data.error === '');
       assert.ok(data.status === 'complete');
-      nockBuild.done();
       return cb();
     });
   }
