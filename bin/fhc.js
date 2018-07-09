afcc1e190271e6612f5c8e158237edee10a705c4
camila de o macedo <cmacedo@camilas-MBP.connect>
2018-07-09 11:35:49 +0100

    fix(help command): RHMAP-19895 - Fix issues and allow use fhc commands --help

--- bin/fhc.js
+++ bin/fhc.js
@@ -7,7 +7,7 @@
   var conf = {_exit: true};
   var argv = process.argv.slice(2);
   argv = (argv.length === 0) ? ['help'] : argv;
-
+  argv = applyHelpCommand(argv);
   fhc.load(conf, function(err, conf) {
     if (err) {
       return errorHandler(err);
@@ -17,3 +17,22 @@
     });
   });
 })();
+
+/**
+ * This fuction was created in order to apply the command fhc help <commands> when the
+ * input is fhc commands --help or fhc commands help. It is checking if the last position is the --help
+ * or help and add it as the first position of the command.
+ * @param argv
+ * @returns {*}
+ */
+function applyHelpCommand(argv) {
+  var lastPosition = argv[argv.length - 1];
+  if (argv.length > 0 && (lastPosition === '--help' || lastPosition === 'help')) {
+    var tmpArgv = ['help'];
+    for (var i = 0; i < argv.length - 1; i++) {
+      tmpArgv.push(argv[i]);
+    }
+    argv = tmpArgv;
+  }
+  return argv;
+}
